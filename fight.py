3a74d074537427bea6c420660064471689a67652
Matias Godoy <matias.godoy@pix4d.com>
2020-11-20 18:10:43 +0100

    Added replays folder. Small fix to add names to bots.

--- fight.py
+++ fight.py
@@ -4,23 +4,26 @@ from sc2.player import Bot
 
 # from bots.season_01.andrea_bot import AndreaBot
 # from bots.season_01.angusin_bot import AngusinBot
-from bots.season_01.basic_v2 import BasicV2
-# from bots.season_01.kikamendor import Kikamendor
-# from bots.season_01.retromonguer_bot import RetromonguerBot
-from bots.season_01.roberto import RobertoElRobot
+# from bots.season_01.basic_v2 import BasicV2
+from bots.season_01.kikamendor import Kikamendor
+from bots.season_01.retromonguer_bot import RetromonguerBot
+# from bots.season_01.roberto import RobertoElRobot
 
 
 def main():
     # Change only this part for the fight
-    bot1 = BasicV2()
-    bot2 = RobertoElRobot()
+    bot1 = Kikamendor()
+    bot2 = RetromonguerBot()
 
     # Do not modify this part
+    bot1_name = bot1.__class__.__name__
+    bot2_name = bot2.__class__.__name__
+
     sc2.run_game(
         sc2.maps.get('AcropolisLE'),
-        [Bot(Race.Terran, bot1), Bot(Race.Terran, bot2)],
+        [Bot(Race.Terran, bot1, name=bot1_name), Bot(Race.Terran, bot2, name=bot2_name)],
         realtime=False,
-        save_replay_as=f'replays/{bot1.__class__.__name__}_vs_{bot2.__class__.__name__}.SC2Replay',
+        save_replay_as=f'replays/{bot1_name}_vs_{bot2_name}.SC2Replay',
         game_time_limit=3600
     )
 
